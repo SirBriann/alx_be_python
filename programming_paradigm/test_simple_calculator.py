852103850ae88c41d254303d000c2a33087b8f4d
SirBriann <bmaina049@yahoo.com>
2025-08-03 21:53:36 +0300

    Writing Unit Tests for a Simple Calculator Class

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,7 +11,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-5, -5), -10)
 
-    def test_subtractaction(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 4), 6)
         self.assertEqual(self.calc.subtract(0, 4), -4)
         self.assertEqual(self.calc.subtract(-3, -7), 4)
